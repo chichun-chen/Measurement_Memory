f5767a71229d5f1807beff3875da923f36dc9e55
Jun7414 <eric08000800@gmail.com>
2023-03-16 15:52:04 +0800

    Molecular H

--- measurement_memory.py
+++ measurement_memory.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pennylane as qml
+from pennylane import numpy as pnp
 
 ## Evaluate H
 def evaluate_eigenstate_MM(sample, H, memory, memory_states=10000):
@@ -14,20 +16,106 @@ def evaluate_eigenstate_MM(sample, H, memory, memory_states=10000):
             
             kHk = 0
             for i,op in enumerate(H.ops):
-                ws = op.wires.tolist()
-                op_val = 1
-                for w in ws: 
-                    op_val *= eigvals[w]
-                kHk += coeffs[i]*op_val
+                if op.name == 'Identity':
+                    kHk += coeffs[i]
+                else: 
+                    ws = op.wires.tolist()
+                    op_val = 1
+                    for w in ws: 
+                        op_val *= eigvals[w]
+                    kHk += coeffs[i]*op_val
             H_val += kHk*v
             memory[k] = kHk
                     
         total += v
     return H_val/total
 
+def evaluate_eigenstate(sample, H):
+    total = 0
+    H_val = 0
+    coeffs = H.coeffs
+    for k,v in sample.items():
+        eigvals = [eigenvalue(c) for c in k]
+        kHk = 0
+        for i,op in enumerate(H.ops):
+            if op.name == 'Identity':
+                kHk += coeffs[i]
+            else:
+                ws = op.wires.tolist()
+                op_val = 1
+                for w in ws:
+                    op_val *= eigvals[w]
+                kHk += coeffs[i]*op_val
+        H_val += kHk*v
+        total += v
+    return H_val/total
+
 def eigenvalue(state:str):
     return int(1-2*int(state))
 
+
+## Hamiltonian grouping 
+def group_H(H):
+    Hg = []
+    for g in H.grouping_indices:
+        obs = [H.ops[i] for i in g]
+        coeffs = [H.coeffs[i] for i in g]
+        Hg.append(qml.Hamiltonian(coeffs, obs, grouping_type='qwc'))
+    return Hg
+
+def get_measurement_list(Hg:list, qubits):
+    Measurement_list = []
+    for g in Hg:
+        basis = [0]*qubits
+        for op in g.ops:
+            wires = op.wires.labels
+            Paulis = op.name
+            if not Paulis == 'Identity':
+                if type(Paulis) == str:
+                    Paulis = [Paulis]
+                
+                for i,obs in enumerate(Paulis):
+                    if basis[wires[i]] == 0 :
+                        basis[wires[i]] = obs
+                    else:
+                        assert(basis[wires[i]] == obs), "Not qubit-wise commuted!"
+        Measurement_list.append(basis)
+    return Measurement_list
+
+def measurement_rotation(M):
+    for i,m in enumerate(M):
+        if m == 'PauliX':
+            qml.Hadamard(i)
+        elif m == 'PauliY':
+            qml.adjoint(qml.S(i))
+            qml.Hadamard(i)
+
+
+# Converter
+def char_to_Pauli(c:str, i:int):
+    if c == 'X':
+        return qml.PauliX(i)
+    elif c == 'Y':
+        return qml.PauliY(i)
+    elif c == 'Z':
+        return qml.PauliZ(i)
+    else:
+        return None
+
+def str_to_Pauli(H:list):
+    ops = []
+    for string in H:
+        obs = None
+        for i,c in enumerate(string):
+            if not obs:
+                obs = char_to_Pauli(c,i)
+            else:
+                if char_to_Pauli(c,i):
+                    obs = obs @ char_to_Pauli(c,i)
+        ops.append(obs)
+    return ops
+        
+            
 
 ## Calculate Gradient
 def finite_difference(cost, x, delta=0.0001):
