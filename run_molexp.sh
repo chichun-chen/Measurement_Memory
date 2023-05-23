95f77a56586740c93e52d1aa2864e89993dfca4f
Jun7414 <eric08000800@gmail.com>
2023-05-23 21:41:20 +0800

    fixbug

--- run_molexp.sh
+++ run_molexp.sh
@@ -3,5 +3,10 @@
 
 for num in {1..10}
 do
-    taskset -c 16-23 python Experiment_Molecular.py -MM $num &
+    taskset -c $[num-1] python Experiment_Molecular.py -MM $num &
+done
+
+for num in {1..10}
+do
+    taskset -c $[num+10] python Experiment_Molecular.py $num &
 done
