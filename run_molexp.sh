ea369a8949babf20d40c17198c3e3734bea946ba
chichun-chen <ccchen4334@cluster.aracluster>
2023-05-30 13:44:20 +0800

    molecule result

--- run_molexp.sh
+++ run_molexp.sh
@@ -3,10 +3,13 @@
 
 for num in {1..10}
 do
-    taskset -c $[num-1] python Experiment_Molecular.py -MM $num &
+    taskset -c 60-69 python Experiment_Molecular.py -MM $num &
 done
 
 for num in {1..10}
 do
-    taskset -c $[num+10] python Experiment_Molecular.py $num &
+    taskset -c 80-89  python Experiment_Molecular.py $num &
 done
+
+#taskset -c 1  python Experiment_Molecular.py 1 &
+#taskset -c 2  python Experiment_Molecular.py -MM 1 &
