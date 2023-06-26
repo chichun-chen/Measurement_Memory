8cf7fe5cb7aced6c30ae70037a2ffcd22d25e4fb
chichun-chen <ccchen4334@cluster.aracluster>
2023-06-26 11:20:25 +0800

    FG results

--- run_exp.sh
+++ run_exp.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 
-for num in {1..5}
+for num in {1..10}
 do
+    python Experiment_Molecular.py -MM $num &
     #taskset -c 0-15 python Experiment_Molecular.py -MM $num &
-    taskset -c 0-15 python Experiment_Ising.py -MM $num &
+    #taskset -c 0-15 python Experiment_Ising.py -MM $num &
 done
 
-for num in {1..5}
+for num in {1..10}
 do
+    python Experiment_Molecular.py $num &
     #taskset -c 16-31  python Experiment_Molecular.py $num &
-    taskset -c 16-31 python Experiment_Ising.py $num &
+    #taskset -c 16-31 python Experiment_Ising.py $num &
 done
 
 #taskset -c 0  python Experiment_Ising.py 1 &
